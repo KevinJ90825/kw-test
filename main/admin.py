e79162cd3b40b26a2769b5b2abd749dfcadb82e2
--get <=>
2017-11-21 13:39:56 -0600

    Admin

--- main/admin.py
+++ main/admin.py
@@ -6,6 +6,7 @@ from main.models import HomeBuyer
 class HomeBuyerAdmin(admin.ModelAdmin):
     model = HomeBuyer
     exclude = ()
+    list_display = ('first_name', 'last_name', 'email')
 
 
 admin.site.register(HomeBuyer, HomeBuyerAdmin)
