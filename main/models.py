2c84c814d7aa08ad4a0e04824f8b04d364d90990
--get <=>
2017-11-21 13:24:50 -0600

    Save form data

--- main/models.py
+++ main/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-class HomeBuyer(models.Object):
+class HomeBuyer(models.Model):
 
     first_name = models.TextField()
     last_name = models.TextField()
 
     email = models.EmailField()
+
+    agent_email = models.EmailField(null=True)
 
     current_address = models.TextField()
     property_address = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from main.models import HomeBuyer
+
+
 def index(request):
+
+    if request.method == "POST":
+        HomeBuyer.objects.create(
+            first_name=request.POST['first_name'],
+            last_name=request.POST['last_name'],
+            email=request.POST['email'],
+            current_address=request.POST['current_address'],
+            property_address=request.POST['property_address'],
+        )
 
     return render(request, 'main/index.html', {})
