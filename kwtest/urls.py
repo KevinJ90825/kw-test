74117eaae623c9d67f8dca4ef7a6ba6c41a4fbdf
--get <=>
2017-11-21 13:51:35 -0600

    Static urls

--- kwtest/urls.py
+++ kwtest/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
 from django.contrib import admin
 from main import views as main_views
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', main_views.index)
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
