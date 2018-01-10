81a0d0402e0843bdc33f674d5859b9831bcd421f
--get <=>
2018-01-10 11:21:55 -0600

    Change upload amount

--- main/views.py
+++ main/views.py
@@ -9,12 +9,14 @@ def index(request):
     if request.method == "POST":
         form = InspectionUploadForm(request.POST, request.FILES)
         if form.is_valid():
+            count = 0
             for uploaded_file in request.FILES.getlist('inspection_file'):
+                count += 1
                 InspectionUpload.objects.create(
                     agent_email=form.cleaned_data['agent_email'],
                     inspection_file=uploaded_file
                 )
-            return render(request, 'main/thanks.html')
+            return render(request, 'main/thanks.html', {'NUM_UPLOADS': count})
 
     else:
         form = InspectionUploadForm()
